50e32b538630d1e0bcec0a86f2ca3e5cd3a2a274
LDQ-first <178848890@qq.com>
2017-12-31 14:27:56 +0800

    控制中间件的调用

--- index.js
+++ index.js
@@ -12,30 +12,41 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use((req, res, next) => {
+  req.middlewares = []
+  next()
+})
+
 const mw1 = (options) => {
   return (req, res, next) => {
    /* if (options.whatEver) {
       console.log('hey')
     }*/
-    console.log(req.body)
-    console.log('mw1')
+    req.middlewares.push('mw1')
     next()
   }
 }
 
 const mw2 = (req, res, next) => {
-  console.log('mw2')
+  req.middlewares.push('mw2')
   next()
 }
 
 const mw3 = (req, res, next) => {
-  console.log('mw3')
-  res.end('done')
+  req.middlewares.push('mw3')
+  res.end(JSON.stringify(req.middlewares))
 }
 
-app.use(mw1(), [mw2, mw3])
+// app.use(mw1(), [mw2, mw3])
 
 // app.use(auth)
+
+app.use('/', mw1())
+
+app.get('/a', mw2)
+app.post('/u', mw2)
+
+app.use(mw3)
 
 app.use((err, req, res, next) => {
   res.end(err)
