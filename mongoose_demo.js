abdc79709a3254db63b287ee6a763bb46a7a2204
LDQ-first <178848890@qq.com>
2018-01-13 21:35:04 +0800

    use mongoose: add

---
+++ mongoose_demo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+mongoose.Promise = Promise
+
+
+
+const url = `mongodb://localhost:27017/what_i_love`
+const connection = mongoose.connect(url, { useMongoClient: true })
+const db = mongoose.connection
+
+
+const Schema = mongoose.Schema
+
+const ObjectId = Schema.Types.ObjectId
+// ObjectId = mongoose.Types.ObjectId
+const UserSchema = new Schema({
+  name: { type: String, require: true,  unique: true, index: 1 }, // index 排序（1 正序， -1 逆序）
+  age: { type: Number, max: 188, min: [0, 'you are too young to be born'] },
+})
+
+const UserModel = mongoose.model('user', UserSchema)
+
+;(async ()=> {
+  const u = await UserModel.create({
+    name: "b",
+    a: 21
+  })
+  return u
+})()
+ .then((r) => {
+   console.log(r)
+ })
+ .catch((e) => {
+   console.log(e)
+ })
+
+db.on('open', () => {
+  console.log('db connected')
+})
+
+db.on('error', () => {
+  console.log(e)
+})
